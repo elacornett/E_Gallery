eac1f8e10fd36bd06e5197c89ffa643d50b8158d
ElaCornett <elacornett@gmail.com>
2018-01-22 11:22:49 -0500

    yellow

---
+++ secret/yellow/scripts/application.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+
+
+
+$(document).ready(
+
+function(){
+	
+	
+	$("#position05").hover(function(){
+	
+		$("#yellow").css("background-image", "url('images/y05.jpg')");
+			
+	});
+	
+	$("#position04").hover(function(){
+	
+		$("#yellow").css("background-image", "url('images/y04.jpg')");
+			
+	});
+	
+		$("#position03").hover(function(){
+	
+		$("#yellow").css("background-image", "url('images/y03.jpg')");
+			
+	});
+	
+		$("#position02").hover(function(){
+	
+		$("#yellow").css("background-image", "url('images/y02.jpg')");
+			
+	});
+	
+		$("#position01").hover(function(){
+	
+		$("#yellow").css("background-image", "url('images/y01.jpg')");
+			
+	});
+	
+	
+//var snarkyComment = "yellow there";
+//
+//$("h1").text(snarkyComment);
+//$("p").text("yellow is my favorite color... not really");
+//
+
+	
+	
+	
+	
+});
+
---
+++ secret/yellow/scripts/application3.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+
+
+
+$(document).ready(
+
+function(){
+	
+	
+	$("#position05").hover(function(){
+	
+		$("#face").css("background-image", "url('images/face01.jpg')");
+			
+	});
+	
+	$("#position04").hover(function(){
+	
+		$("#face").css("background-image", "url('images/face02.jpg')");
+			
+	});
+	
+		$("#position03").hover(function(){
+	
+		$("#face").css("background-image", "url('images/face03.jpg')");
+			
+	});
+	
+		$("#position02").hover(function(){
+	
+		$("#face").css("background-image", "url('images/face04.jpg')");
+			
+	});
+	
+		$("#position01").hover(function(){
+	
+		$("#face").css("background-image", "url('images/face05.jpg')");
+			
+	});
+	
+	
+//var snarkyComment = "I hate you";
+//
+//$("h1").text(snarkyComment);
+//$("p").text("You are as smart as you are dumb");
+//
+
+	
+	
+	
+	
+});
+
